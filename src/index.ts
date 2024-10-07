f9d6cf1623209241a22f83d8a08ccaa5016f2483
hi-se <40375981+hi-se@users.noreply.github.com>
2024-10-07 15:09:47 +0900

    feat: export notion-api

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * as ResponseProperties from "./response-properties-schema.js";
 export * as RequestProperties from "./request-properties-schema.js";
 export * as ResponsePropertiesRetriever from "./response-properties-retrievers.js";
 export * from "./helper.js";
+export * from "./notion-api.js";
